76a6239c3cf04f01c7c76a246a28d688d18a2e80
村上幸雄 <yukio@mbpci7.local>
2016-03-06 23:30:48 +0900

    first commit

---
+++ Brownout/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Brownout
+//
+//  Created by 村上幸雄 on 2016/03/06.
+//  Copyright © 2016年 MURAKAMI Yukio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
